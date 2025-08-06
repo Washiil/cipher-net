0aef5cd9d572e7b5baa8e8b0e86d0001ba56acd6
Washi <65116802+Washiil@users.noreply.github.com>
2025-08-05 20:42:25 -0400

    basic scraper implemented

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
+
+type player struct {
+	uuid   string
+	twitch string
+}
 
 func main() {
 	region := flag.String("region", "na", "Leaderboard to Scrape")
@@ -22,6 +31,31 @@ func main() {
 		fmt.Println(" > Verbose mode: ON")
 	} else {
 		fmt.Println(" > Verbose mode: OFF")
+	}
+
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+	)
+
+	c.OnHTML("td a[href*='/valorant/profile']", func(e *colly.HTMLElement) {
+		username := e.ChildText("span.v3-trnign .max-w-full.truncate")
+		tag := e.ChildText("span.v3-trnign__discriminator")
+
+		twitchURL := e.DOM.Parent().Parent().Find(`a[aria-label="Visit twitch profile"]`).AttrOr("href", "")
+		twitchName := ""
+		if twitchURL != "" {
+			parts := strings.Split(twitchURL, "/")
+			twitchName = parts[len(parts)-1]
+		}
+
+		fmt.Printf("Username: %s\n", username)
+		fmt.Printf("Tag: %s\n", tag)
+		fmt.Printf("Twitch: %s\n", twitchName)
+	})
+
+	err := c.Visit("https://tracker.gg/valorant/leaderboards/ranked/all/default?platform=pc&region=na&act=ac12e9b3-47e6-9599-8fa1-0bb473e5efc7&page=1")
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("\n--- Execution Complete ---")
